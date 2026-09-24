bda6d8e96b738821f7680714f7435816707c25f1
Abhay-K-Kashyap <abjkashyap@gmail.com>
2026-09-24 18:24:26 +0530

    added ingestion-service and data simulation and started kafka

--- ingestion-service/src/main/java/com/abhay/ingestion_service/dto/EnergyUsageDto.java
+++ ingestion-service/src/main/java/com/abhay/ingestion_service/dto/EnergyUsageDto.java
@@ -1,9 +1,11 @@
 package com.abhay.ingestion_service.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+
 import java.time.Instant;
 
-
+@Builder
 public record EnergyUsageDto (
     Long deviceId,
     double energyConsumed,
